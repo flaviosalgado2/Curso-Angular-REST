738c9e3606c69d5044050d53f0d9bf31e33ed3f0
flaviosalgado2 <flaviosalgado2@hotmail.com>
2021-06-14 23:13:03 -0300

    servico de login jwt implementado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+//adicionado um sevico
+import { LoginServiceService } from './service/login-service.service';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Curso-Angular-REST';
+
+  //atributos criados
+  usuario = { login: '', senha: '' };
+
+  constructor(private loginService: LoginServiceService){}
+
+  //metodo criado
+  public login() {
+    //console.log("Teste Login: " + this.usuario.login + " senha: " + this.usuario.senha);
+    this.loginService.login(this.usuario);
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+//adicionado - com isso dar pra trabalhar com submit de formularios
+import { FormsModule } from '@angular/forms';
+
+//adicionado - para fazer requisicoes AJAX
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
